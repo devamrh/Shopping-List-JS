868c34bfcc5989e0f06c1e770e0aa019c92d2ef0
A. M. Rafinul Huq <amrh.cse@gmail.com>
2023-11-22 01:48:47 +0600

    second commit

--- script.js
+++ script.js
@@ -12,23 +12,50 @@ const addItem = (e) =>{
     
     const newItem = itemInput.value;
 
-
     if(itemInput.value == ''){
         alert("Please enter an item");
         return;
     }
 
+    // Create a list
     const li = document.createElement('li');
     li.appendChild(document.createTextNode(newItem));
 
-    
+    const button = createButton('remove-item btn-link text-red');
+    li.appendChild(button);
+
+    itemList.appendChild(li);
+
+    itemInput.value = '';
+
+    // console.log(itemList)
+    // console.log(button)
     // console.log(li)
     // console.log('success');
-
-    
 }
 
 
+// Secondary Function
+
+const createButton = (classes) =>{
+    const button = document.createElement('button');
+    button.className = classes;
+    const icon = createIcon('fa-solid fa-xmark');
+    button.appendChild(icon);
+    return button;
+
+    // console.log(button)
+
+}
+
+
+
+// Third Function
+const createIcon = (classes) =>{
+    const icon = document.createElement('i');
+    icon.className = classes;
+    return icon;
+}
 
 
 
